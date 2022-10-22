bc7552c7a46c97fa52f99f2cb1f945dd0a1ed939
Jake O'Donnell <10jakeo@gmail.com>
2022-10-22 09:45:54 -0400

    Working project with leaderboard

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, session, redirect, request
 import random
 app = Flask(__name__)
-app.secret_key = 'segkjnbt5noisgnos5'
+app.secret_key = 'asjabnfaklbjrgvl'
+session_cookies_secure = True
 
 @app.route('/')
 def game():
+    session['score'] = []
     session['guess'] = []
     session['guesses'] = 0
-    session['random'] = random.randint(1, 100)
+    session['random'] = random.randint(1, 2)
+    return render_template('game.html')
+
+@app.route('/replay')
+def replay():
+    session['guesses'] = 0
+    session['guess'] = []
     return render_template('game.html')
 
 @app.route('/guess', methods = ['POST'])
@@ -41,7 +49,12 @@ def lose():
 
 @app.route('/leader_board', methods = ['POST'])
 def leader():
-    session['score'].append([request.form['name'],session['guesses']])
+    print(session['score'])
+    if session['score'] == []:
+        session['score'] = [[request.form['name'],session['guesses']]]
+    else:
+        session['score'].append([request.form['name'],session['guesses']])
+        session['score'] = session['score']
     print(session['score'])
     return redirect('/win_2')
 
